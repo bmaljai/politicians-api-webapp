91f637f3aa0e5989e0d141127452dcc03a8245f5
Bejan Maljai <bmaljai@gmail.com>
2016-05-23 13:25:42 -0700

    namespaced versioning to api

---
+++ app/controllers/api/v1/politicians_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::PoliticiansController < ApplicationController
+  def index
+    @politicians = Politician.all
+  end
+
+  def show
+    @politician = Politician.find(params[:id])
+  end
+
+  def create
+    firstname = params[:first_name]
+    lastname = params[:last_name]
+    birthdate = params[:birthdate]
+    totalyears = params[:total_years_service] 
+    party = params[:party]
+    currentoffice = params[:current_office]
+
+    @politician = Politician.new(first_name: firstname, last_name: lastname, birthdate: birthdate, total_years_service: totalyears, party: party, current_office: currentoffice )
+    @politician.save
+
+    render :show
+  end
+
+  def update
+    @politician = Politician.find(params[:id])
+    firstname = params[:first_name] || @politician.first_name
+    lastname = params[:last_name] || @politician.last_name
+    birthdate = params[:birthdate] || @politician.birthdate
+    totalyears = params[:total_years_service] || @politician.total_years_service
+    party = params[:party] || @politician.party
+    currentoffice = params[:current_office] || @politician.current_office
+
+    @politician.update(first_name: firstname, last_name: lastname, birthdate: birthdate, total_years_service: totalyears, party: party, current_office: currentoffice )
+
+    render :show
+
+  end
+
+  def destroy
+    @politician = Politician.find(params[:id])
+    @politician.delete
+
+    render json: @politician
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get '/politicians', to: 'politicians#index'
-  post '/politicians', to: 'politicians#create'
-  get '/politicians/:id', to: 'politicians#show'
-  patch '/politicians/:id', to: 'politicians#update'
-  delete '/politicians/:id', to: 'politicians#destroy'
+  namespace :api do
+    namespace :v1 do
+      get '/politicians', to: 'politicians#index'
+      post '/politicians', to: 'politicians#create'
+      get '/politicians/:id', to: 'politicians#show'
+      patch '/politicians/:id', to: 'politicians#update'
+      delete '/politicians/:id', to: 'politicians#destroy'
+    end
+  end
+
+
 end
